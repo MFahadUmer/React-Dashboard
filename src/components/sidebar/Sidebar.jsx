f29610aebafb5b12621a4e53a78ed6d559b78261
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2023-01-24 16:19:32 +0500

    Adds Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../../imgs/logo.png';
 import './Sidebar.css';
-
+import { SidebarData } from '../../Data/Data';
+import { UilSignOutAlt } from '@iconscout/react-unicons';
 const Sidebar = () => {
+  const [selected, setSelected] = useState(1);
   return (
     <div className="Sidebar">
       {/* Logo */}
@@ -14,11 +16,20 @@ const Sidebar = () => {
       </div>
       {/* Menu */}
       <div className="menu">
+        {SidebarData.map((item, index) => {
+          return (
+            <div
+              className={selected === index ? 'menuItem active' : 'menuItem'}
+              key={index}
+              onClick={() => setSelected(index)}
+            >
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
+        })}
         <div className="menuItem">
-          <div>
-            Icon
-            <span>Dashboard</span>
-          </div>
+          <UilSignOutAlt />
         </div>
       </div>
     </div>
